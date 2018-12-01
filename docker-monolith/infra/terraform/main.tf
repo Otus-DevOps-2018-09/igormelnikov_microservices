787ab184c6cf2518be2085ab7213df455d574132
Igor Melnikov <prodwiththeprod@gmail.com>
2018-12-02 02:30:07 +0300

    Split ansible playbook into several files

--- docker-monolith/infra/terraform/main.tf
+++ docker-monolith/infra/terraform/main.tf
@@ -39,5 +39,5 @@ resource "google_compute_firewall" "firewall_puma" {
 }
 
 output "host_external_ip" {
-  value = "${google_compute_instance.app.network_interface.0.access_config.0.assigned_nat_ip}"
+  value = "${google_compute_instance.docker-host.*.network_interface.0.access_config.0.assigned_nat_ip}"
 }
