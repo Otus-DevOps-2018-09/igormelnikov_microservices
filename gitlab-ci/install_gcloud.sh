f0db4085692e628b7a0506e856e556e15adbce72
Igor Melnikov <prodwiththeprod@gmail.com>
2018-12-20 00:18:43 +0300

    removed extra line

--- gitlab-ci/install_gcloud.sh
+++ gitlab-ci/install_gcloud.sh
@@ -8,4 +8,3 @@ wget https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.tar.gz
 tar zxvf google-cloud-sdk.tar.gz && ./google-cloud-sdk/install.sh --usage-reporting=false --path-update=true
 google-cloud-sdk/bin/gcloud --quiet components update
 google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
-docker login -u _json_key --password-stdin https://us.gcr.io < ${HOME}/gcloud-service-key.json
