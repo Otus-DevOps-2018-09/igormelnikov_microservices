deebffc9d822aecb771ca2d4535384ad448c6e8e
Igor Melnikov <prodwiththeprod@gmail.com>
2018-12-19 23:26:36 +0300

    Added terraform

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+provider "google" {
+  version = "1.19.1"
+  project = "${var.project}"
+  region  = "${var.region}"
+}
+
+resource "google_compute_instance" "machine" {
+  name         = "${var.env_name}"
+  machine_type = "n1-standard-1"
+  zone = "${var.zone}"
+  tags = ["http-server", "https-server"]
+  labels = {
+    "${var.env_name}" = ""
+  }
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1604-lts"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config = {
+    }
+  }
+}
